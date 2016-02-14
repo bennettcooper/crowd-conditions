ed0617986e074ce2509624de3b83545eaff33e2d
Carson Woods <cwoods289@gmail.com>
2016-02-13 21:47:29 -0600

    Refined map and added search with autocomplete functions

--- js/map.js
+++ js/map.js
@@ -2,15 +2,13 @@ function initMap() {
 
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.768874, lng: -122.482981},
-    //scrollwheel: false,
+    center: {lat: 37.768874, lng: -122.482981}, //San Francisco
+    scrollwheel: true,
     zoom: 17
   });
 
+  var input = (document.getElementById('pac-input'));
 
-
-  // Specify features and elements to define styles.
-  //var styleArray = [];
   var pos;
   //grabs current location
   if (navigator.geolocation) {
@@ -19,8 +17,9 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      console.log(map);
+
       map.setCenter(pos);
     });
   }
+  search(map);
 }
---
+++ js/search.js
@@ -0,0 +1,56 @@
+function search(map) {
+
+  var input = (document.getElementById('pac-input'));
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+
+    //if the place searched for does not exist then it displays does not exist
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+
+    //if the place exists it changes map location to display location
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
+}

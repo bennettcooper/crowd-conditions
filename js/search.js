6592bce01fdc2d2b5b38e7e9a2f1b3885f5e8f9f
Carson Woods <cwoods289@gmail.com>
2016-02-13 21:58:07 -0600

    Removed markers on maps and cleaned code

--- js/search.js
+++ js/search.js
@@ -8,19 +8,14 @@ function search(map) {
   autocomplete.bindTo('bounds', map);
 
   var infowindow = new google.maps.InfoWindow();
-  var marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-  });
 
   autocomplete.addListener('place_changed', function() {
     infowindow.close();
-    marker.setVisible(false);
     var place = autocomplete.getPlace();
 
     //if the place searched for does not exist then it displays does not exist
     if (!place.geometry) {
-      window.alert("Autocomplete's returned place contains no geometry");
+      window.alert("That place cannot be found");
       return;
     }
 
@@ -31,15 +26,6 @@ function search(map) {
       map.setCenter(place.geometry.location);
       map.setZoom(17);  // Why 17? Because it looks good.
     }
-    marker.setIcon(/** @type {google.maps.Icon} */({
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(35, 35)
-    }));
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
 
     var address = '';
     if (place.address_components) {
@@ -49,8 +35,5 @@ function search(map) {
         (place.address_components[2] && place.address_components[2].short_name || '')
       ].join(' ');
     }
-
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    infowindow.open(map, marker);
   });
 }
